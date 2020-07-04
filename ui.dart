a775380c7845e8940b17b4c1d697f6933a19dbae
Akram Izzeldin <akram.izz.kh@gmail.com>
2020-07-04 23:30:42 +0200

    implement RenderObjects tree with layout phase

--- ui.dart
+++ ui.dart
@@ -1,5 +1,13 @@
+import 'geometry.dart';
+
 class Window {
   Window._() {}
+
+  Size get physicalSize => _physicalSize;
+  Size _physicalSize = const Size(640.0, 480.0);
+
+  double get devicePixelRation => _devicePixelRation;
+  double _devicePixelRation = 1.0;
 
   scheduleFrame() {
     Future.delayed(Duration(seconds: 1), () => _onDrawFrame(0));
